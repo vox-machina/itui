3373160945b7576f050430589a5d66517bafbd96
Ross McDonald <85923+rossajmcd@users.noreply.github.com>
2024-01-21 22:11:00 +0000

    add card component

--- src/ui/components.clj
+++ src/ui/components.clj
@@ -2,10 +2,18 @@
   (:require [ten-d-c.hiccup-server-components.core :refer [reg-component]]))
 
 (reg-component
- :ux.layouts/navbar-simple
- (fn [{:keys [link-root title nav session]}]
+ :ui.l/navbar-simple
+ (fn [{:keys [link-root nav session] :or {session nil link-root nil nav {}}} title]
    [:nav.navbar.navbar-expand-md.navbar-light.fixed-top.bg-light
+    [:div.container-fluid
     [:a {:class "navbar-brand" :href "/"} title]
     [:button {:class "navbar-toggler" :type "button" :data-toggle "collapse" :data-target "#navbarCollapse" :aria-controls "navbarCollapse" :aria-expanded "false" :aria-label "Toggle navigation"}
      [:span {:class "navbar-toggler-icon"}]]
-    [:div {:class "collapse navbar-collapse" :id "navbarCollapse"}]]))
+    [:div {:class "collapse navbar-collapse" :id "navbarCollapse"}]]]))
+
+(reg-component
+ :ui.l/card
+ (fn [{:keys [session link-root] :or {session nil link-root nil}} title & body]
+   [:div.card
+     [:div.card-header [:h2 title]]
+     [:div.card-body body]]))
---
+++ src/ui/layout.clj
@@ -0,0 +1,11 @@
+(ns ui.layout
+  (:require [io.pedestal.http :as http]
+            [io.pedestal.http.body-params :refer [body-params]]
+            [io.pedestal.http.ring-middlewares :as mw]
+            [ring.middleware.session.cookie :as cookie]
+            [ring.util.response :refer [response]]
+            [ten-d-c.hiccup-server-components.core :refer [->html]]))
+
+(def ses-tors [(mw/session {:store (cookie/cookie-store)}) mw/params mw/keyword-params (body-params) http/html-body])
+
+(defn page [session head body & markup] (response (->html [:ux/html5-doc (head) (body session markup)])))
