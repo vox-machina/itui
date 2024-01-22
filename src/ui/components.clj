d1dd81a88953af30d9f0275b5c99a87ad6c91d64
Ross McDonald <85923+rossajmcd@users.noreply.github.com>
2024-01-22 19:07:03 +0000

    add namespace and review comments

--- src/ui/components.clj
+++ src/ui/components.clj
@@ -1,23 +1,27 @@
 (ns ui.components
+  "Abstract away layout and other Hiccup components.
+  Additionally presents the possibility of abstracting away CSS and styling details."
   (:require [ten-d-c.hiccup-server-components.core :refer [reg-component]]))
 
 ;;;; Layouts - navbars, cards etc.
 ;;;; ===========================================================================
 
+;; REVIEW: plug in parameterise styling to support Bootstrap and other options
 (reg-component :ui.l/navbar-simple
- (fn [{:keys [link-root nav session] :or {session nil link-root nil nav {}}} title]
-   [:nav.navbar.navbar-expand-md.navbar-light.fixed-top.bg-light
-    [:div.container-fluid
-     [:a {:class "navbar-brand" :href "/"} title]
-     [:button {:class "navbar-toggler" :type "button" :data-toggle "collapse" :data-target "#navbarCollapse" :aria-controls "navbarCollapse" :aria-expanded "false" :aria-label "Toggle navigation"}
-      [:span {:class "navbar-toggler-icon"}]]
-     [:div {:class "collapse navbar-collapse" :id "navbarCollapse"}]]]))
+  (fn [{:keys [link-root nav session] :or {session nil link-root nil nav {}}} title]
+    [:nav.navbar.navbar-expand-md.navbar-light.fixed-top.bg-light
+     [:div.container-fluid
+      [:a {:class "navbar-brand" :href "/"} title]
+      [:button {:class "navbar-toggler" :type "button" :data-toggle "collapse" :data-target "#navbarCollapse" :aria-controls "navbarCollapse" :aria-expanded "false" :aria-label "Toggle navigation"}
+       [:span {:class "navbar-toggler-icon"}]]
+      [:div {:class "collapse navbar-collapse" :id "navbarCollapse"}]]]))
 
+;; REVIEW: plug in parameterised styling to support Bootstrap and other optoins
 (reg-component :ui.l/card
- (fn [{:keys [session link-root] :or {session nil link-root nil}} title & body]
-   [:div.card
+  (fn [{:keys [session link-root] :or {session nil link-root nil}} title & body]
+    [:div.card
      [:div.card-header [:h2 title]]
-    [:div.card-body body]]))
+     [:div.card-body body]]))
 
 ;;;; Components - including alerts etc.
 ;;;; ===========================================================================
