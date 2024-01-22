b5df1ef96d672ead1232f12b25f245a9bb2a66e1
Ross McDonald <85923+rossajmcd@users.noreply.github.com>
2024-01-22 18:17:25 +0000

    add functions for converting html to hiccup

--- src/ui/layout.clj
+++ src/ui/layout.clj
@@ -4,10 +4,36 @@
             [io.pedestal.http.ring-middlewares :as mw]
             [ring.middleware.session.cookie :as cookie]
             [ring.util.response :refer [response]]
-            [ten-d-c.hiccup-server-components.core :refer [->html]]))
+            [net.cgrand.enlive-html :refer [html-resource]]
+            [ten-d-c.hiccup-server-components.core :refer [->html]])
+  (:import java.io.StringReader))
 
-(def htm-tors [(body-params) http/html-body])
+(defn- enlive->hiccup
+  [el]
+  (if-not (string? el)
+    (->> (map enlive->hiccup (:content el))
+         (concat [(:tag el) (:attrs el)])
+         (keep identity)
+         vec)
+    el))
 
-(def ses-tors [(mw/session {:store (cookie/cookie-store)}) mw/params mw/keyword-params (body-params) http/html-body])
+(defn- html->enlive [html] (first (html-resource (StringReader. html))))
 
-(defn page [session head body & markup] (response (->html [:ux/html5-doc (head) (body session markup)])))
+(defn html->hiccup
+  "Converts HTML to Hiccup to be used dynamically in programs.
+  This is useful when loading static HTML from disk for example."
+  [html] (-> html html->enlive enlive->hiccup))
+
+(def htm-tors
+  "An interceptor for sessionless HTML generating Pedestal routes."
+  [(body-params) http/html-body])
+
+
+(def ses-tors
+  "An interceptor for sessioned HTML generating Pedestal routes where a cookie store session will suffice."
+  [(mw/session {:store (cookie/cookie-store)}) mw/params mw/keyword-params (body-params) http/html-body])
+
+(defn page
+  "Creates a page template given a session, head, body and variadic forms for content inside body.
+  Using hiccup server components provisions for componentisation."
+  [session head body & markup] (response (->html [:ux/html5-doc (head) (body session markup)])))
