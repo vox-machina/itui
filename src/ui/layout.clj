a217b49013da514051592716d11af25dd405d0c9
Ross McDonald <85923+rossajmcd@users.noreply.github.com>
2024-01-22 07:39:03 +0000

    add html interceptor for page layouts

--- src/ui/layout.clj
+++ src/ui/layout.clj
@@ -6,6 +6,8 @@
             [ring.util.response :refer [response]]
             [ten-d-c.hiccup-server-components.core :refer [->html]]))
 
+(def htm-tors [(body-params) http/html-body])
+
 (def ses-tors [(mw/session {:store (cookie/cookie-store)}) mw/params mw/keyword-params (body-params) http/html-body])
 
 (defn page [session head body & markup] (response (->html [:ux/html5-doc (head) (body session markup)])))
